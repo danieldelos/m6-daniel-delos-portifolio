c04a223405c9a1f761934aee0afa965403ae16c9
daniel delos silva <apeiron52@gmail.com>
2023-03-29 22:51:50 -0300

    feat: portifolio-v-01

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,6 +1,6 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "samuelleao",
-  whatsappNumber: "+5541111111111",
+  nameUser: "Daniel Barbosa da Silva",
+  githubUser: "danieldelos",
+  linkedinUser: "daniel-silva-622607162",
+  whatsappNumber: "+5562985144457",
 };
